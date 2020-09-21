2698348f2b64e4f5fbe0693ae05d6b8edcd7a62e
Nicola Toscan <nicolatoscan99@outlook.it>
2020-09-21 02:21:12 +0200

    Multisala (not tested)

--- src/bot.ts
+++ src/bot.ts
@@ -6,7 +6,6 @@ dotenv.config();
 class Bot {
 
     private bot: Client = null;
-    private sala: Sala = null;
 
     constructor() {
         this.bot = new Client();
@@ -29,67 +28,82 @@ class Bot {
         let params: string[] = msg.content.split(" ");
 
         if (params[0] === 'help') {
-            msg.reply("\n`ping` - Pingami\n`sala <nr ore> <nr posti>` - Crea una sala\n`biglietto` - Ottieni un biglietto\n`chiudi` - Chiudi la sala")
+            this.helpMsg(msg, params);
         } else if (params[0] === 'ping') {
-            msg.reply("", { files: ["./src/muto.mp4"] })
+            this.pingMsg(msg, params);
         } else if (params[0] === 'biglietto') {
-            //BIGLIETTO
-            if (this.sala == null)
-                msg.reply(`La sala non esiste.`);
-            else if (!this.sala.reserve(msg.member))
-                msg.reply('La sala è piena');
-            else
-                msg.reply(`Ecco il biglietto.`);
-
-            return;
+            this.bigliettoMsg(msg, params);
         } else if (params[0] === 'sala') {
-            //SALA
-            if (this.sala !== null) {
-                msg.reply('Il multisala ha finito le sale');
-                return;
-            }
-
-            if (params.length >= 2) {
-                let h: number = +params[1];
-                let posti: number = +params[2];
-
-                if (h > 60) {
-                    msg.reply('Errore: la sala può essere al massimo di 6 ore');
-                    return;
-                }
-
-
-                this.sala = new Sala(posti, msg.guild.roles.cache.find(role => role.name == "biglietto"));
-
-                setTimeout(() => {
-                    if (this.sala !== null) {
-                        this.sala.closeSala();
-                        this.sala = null;
-                    }
-                }, h * 60 * 60 * 1000)
-
-                msg.reply(`La sala da ${posti} posti è stata creata per ${h} ore.`);
-
-            } else {
-                msg.reply('Errore: usa `sala <nr ore> <nr posti>`');
-            }
-
-            return;
+            this.salaMsg(msg, params);
         } else if (params[0] === 'chiudi') {
-            //CHIUDI
-            if (this.sala === null) {
-                msg.reply("Nessuna sala trovata");
-            } else if (!this.sala.isInSala(msg.member)) {
-                msg.reply("Devi essere nella sala per poter chiuderla");
-            } else {
-                this.sala.closeSala();
-                this.sala = null;
-                msg.reply("La sala è stata chiusa");
-            }
-            return;
+            this.chiudiMsg(msg, params);
         }
 
     }
+
+    private helpMsg(msg: Message, params: string[]) {
+        msg.reply("\n`ping` - Pingami\n`sala <nr ore> <nr posti>` - Crea una sala\n`biglietto` - Ottieni un biglietto\n`chiudi` - Chiudi la sala")
+    }
+
+    private pingMsg(msg: Message, params: string[]) {
+        msg.reply("", { files: ["./src/muto.mp4"] })
+    }
+
+    private bigliettoMsg(msg: Message, params: string[]) {
+        
+        
+        if (params.length >= 1) {
+            let nr = +params[1];
+
+            let sala = Sala.sale.find(s => s.getIndex() == nr);
+            if (!sala) {
+                msg.reply('Errore: Sala non trovata');
+                return;
+            }
+            if (sala.reserve(msg.member)) {
+                msg.reply('Errore: La sala è piena');
+                return;
+            }
+
+            msg.reply(`Ecco il biglietto per la sala ${nr}.`);
+
+        } else {
+            msg.reply('Errore: usa `biglietto <numero sala>`');
+        }
+        
+    }
+
+    private async salaMsg(msg: Message, params: string[]) {
+        if (Sala.sale.length >= 100) {
+            msg.reply('Il multisala ha finito le sale');
+            return;
+        }
+
+        if (params.length >= 2) {
+            let h: number = +params[1];
+            let posti: number = +params[2];
+
+            if (h > 6) {
+                msg.reply('Errore: la sala può essere al massimo di 6 ore');
+                return;
+            }
+
+
+            let sala = new Sala(posti, msg.guild, h);
+            await sala.setUp();
+            Sala.sale.push(sala);
+            msg.reply(`La sala ${sala.getIndex()} da ${posti} posti è stata creata per ${h} ore.`);
+
+        } else {
+            msg.reply('Errore: usa `sala <nr ore> <nr posti>`');
+        }
+    }
+
+    private chiudiMsg(msg: Message, params: string[]) {
+        msg.reply("TODO");
+    }
+
+
 
 }
 
--- src/sala.ts
+++ src/sala.ts
@@ -1,14 +1,52 @@
-import { GuildMember, Role } from "discord.js";
+import { Guild, GuildMember, Role, VoiceChannel } from "discord.js";
 
 export default class Sala {
-    private max: number = 0;
-    private members: GuildMember[] = [];
-    private role: Role = null;
 
-    constructor(max: number, role: Role) {
+    public static sale: Sala[] = [];
+    private static currIndex = 0;
+
+    private index: number = 0;
+    private max: number = 0;
+    private guild: Guild = null;
+    private role: Role = null;
+    private channel: VoiceChannel = null;
+    private members: GuildMember[] = [];
+
+
+    constructor(max: number, guild: Guild, lenghtH: number) {
+        Sala.currIndex = (Sala.currIndex + 1) % 100;
+        this.index = Sala.currIndex;
         this.max = max;
-        this.role = role;
+        this.guild = guild;
+
+        setTimeout(() => {
+            this.closeSala();
+        }, lenghtH * 1000 * 60 * 60)
+
     }
+
+    public async setUp() {
+
+        this.role = await this.guild.roles.create({
+            data: {
+                name: `Biglietto Sala ${this.index}`,
+                color: 'BLUE'
+            }
+        });
+
+        this.channel = await this.guild.channels.create(`Sala ${this.index}`, {
+            type: 'voice',
+            permissionOverwrites: [{
+                id: this.guild.id,
+                allow: ['CONNECT']
+            },{
+                id: this.role.id,
+                allow: ['CONNECT']
+            }]
+        })
+    }
+
+
 
     public reserve(member: GuildMember): boolean {
         if (this.members.length < this.max && this.members.indexOf(member) < 0) {
@@ -19,15 +57,19 @@ export default class Sala {
         return false;
     }
 
-
-
     public closeSala(): void {
         this.members.forEach(m => {
             m.roles.remove(this.role);
         })
+        this.channel.delete();
+        Sala.sale = Sala.sale.filter(s => s != null)
     }
 
     public isInSala(m: GuildMember): boolean {
         return this.members.indexOf(m) >= 0;
+    }
+
+    public getIndex(): number {
+        return this.index;
     }
 }
